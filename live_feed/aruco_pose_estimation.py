15ebc67e3588c65de9bc1336d7bfa50647ad9194
Gianluca-Cientanni <40888595+Gianluca-Cientanni@users.noreply.github.com>
2018-08-12 15:50:22 +0100

    aesthetic minor

--- live_feed/aruco_pose_estimation.py
+++ live_feed/aruco_pose_estimation.py
@@ -5,25 +5,23 @@ import glob
 import cv2.aruco as aruco
 import time
 
-
 # This code is developed from aruco_detection_test, but adding the pose estimation.
 # The first section is calibration, the second section is marker recognition and
 # pose estimation. Here you will need to take your calibration images yourself and
 # save them as img1, img2, img3 etc.
 
-
 ###########################
 # SECTION 1 - CALIBRATION #
 ##########################
 
-objpoints = []  # 3D point in real world space where chess squares are
-imgpoints = []  # 2D point in image plane, determined by CV2
+objpoints = []
+imgpoints = []
 
-CHESSBOARD_CORNERS_ROWCOUNT = 9
-CHESSBOARD_CORNERS_COLCOUNT = 6
+chessboard_row = 9
+chessboard_col = 6
 
-objp = np.zeros((CHESSBOARD_CORNERS_ROWCOUNT * CHESSBOARD_CORNERS_COLCOUNT, 3), np.float32)
-objp[:, :2] = np.mgrid[0:CHESSBOARD_CORNERS_ROWCOUNT, 0:CHESSBOARD_CORNERS_COLCOUNT].T.reshape(-1, 2)
+objp = np.zeros((chessboard_row * chessboard_col, 3), np.float32)
+objp[:, :2] = np.mgrid[0:chessboard_row, 0:chessboard_col].T.reshape(-1, 2)
 
 images = glob.glob('img*.png')  # Calibration images
 imageSize = None
@@ -32,11 +30,10 @@ for iname in images:
     img = cv2.imread(iname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    board, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), None)
+    board, corners = cv2.findChessboardCorners(gray, (chessboard_row, chessboard_col), None)
 
     if board == True:
         objpoints.append(objp)
-
 
         corners_acc = cv2.cornerSubPix(
             image=gray,
@@ -47,31 +44,28 @@ for iname in images:
                       0.001))  # Last parameter is about termination critera
         imgpoints.append(corners_acc)
 
-
         if not imageSize:
             imageSize = gray.shape[::-1]
 
-        img = cv2.drawChessboardCorners(img, (CHESSBOARD_CORNERS_ROWCOUNT, CHESSBOARD_CORNERS_COLCOUNT), corners_acc,
+        img = cv2.drawChessboardCorners(img, (chessboard_row, chessboard_col), corners_acc,
                                         board)
 
         cv2.imshow('Chessboard', img)
         cv2.waitKey(0)
     else:
-        print("Not able to detect a chessboard in image: {}".format(iname))
+        print("Not able to detect a chessboard in image")
 
 cv2.destroyAllWindows()
 
 if len(images) < 1:
     print(
-        "Calibration was unsuccessful. No images of chessboards were found. Add images of chessboards and use or "
-        "alter the naming conventions used in this file.")
+        "Calibration was unsuccessful.")
     exit()
 
 if not imageSize:
 
     print(
-        "Calibration was unsuccessful. We couldn't detect chessboards in any of the images supplied."
-        " Try changing the patternSize passed into findChessboardCorners(), or try different pictures of chessboards.")
+        "Calibration was unsuccessful.")
     exit()
 
 calibration, cameraMatrix, distCoeffs, rvecs, tvecs = cv2.calibrateCamera(
@@ -87,13 +81,11 @@ print(distCoeffs)
 f = open('calibration.pckl', 'wb')
 pickle.dump((cameraMatrix, distCoeffs, rvecs, tvecs), f)
 f.close()
-print('Calibration successful. Calibration file used: {}'.format('calibration.pckl'))
-
+print('Calibration successful.')
 
 #####################################################
 # SECTION 2 - MARKER AQUISITION AND POSE ESTIMATION #
 ####################################################
-
 
 cap = cv2.VideoCapture(0)   # The argument of VideoCapture is the port number that the camera you wish to use
                             # is occupying. The standard is 0 and should be sufficient if no other cameras
